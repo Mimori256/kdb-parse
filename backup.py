16ac3e8ff5e3dca98e5deb1855ea153202af1262
Mimori256 <mimorimori256@gmail.com>
2021-09-28 16:10:29 +0900

    update

---
+++ backup.py
@@ -0,0 +1,358 @@
+import sys
+
+#Convert CSV to JSON for twinc
+
+class Class():
+
+    def __init__(self, class_id, name, module, period, room, description):
+        self.class_id = class_id
+        self.name = name
+        self.module = module
+        self.period = period
+        self.room = room
+        self.description = description
+
+    
+# Global consts
+global WEEKDAY_LIST 
+WEEKDAY_LIST =  ["月","火","水","木","金","土"]
+
+
+def create_class_list():
+
+    with open("tmp.csv", "r", encoding="utf_8") as f:
+        class_list = []
+        lines = f.readlines()
+
+      # Remove the header
+        lines.pop(0)
+
+        for line in lines:
+          tmp = line.split('"')
+          class_id = tmp[1]
+          name = tmp[3]
+          module = tmp[11]
+          period = tmp[13]
+          room = tmp[15]
+          description = tmp[21]
+
+          if room == "":
+              room = " " 
+        
+          # Remove classes that are not opened in this year
+          if not "" in set([class_id, name, module, period, room, description]):
+              class_list.append(Class(class_id, name, module, period, room, description))
+
+        return class_list
+
+
+def is_multiple_terms(module):
+
+    if module.find("春") != -1 and module.find("秋") != -1:
+        return True
+
+    else:
+        return False
+
+
+def days_count(period):
+
+    count = 0
+
+    for s in period:
+        if s in WEEKDAY_LIST :
+            count += 1
+
+    return count
+    
+
+def remove_special_module(module):
+
+    remove_list = [" 夏季休業中", " 春季休業中", "夏季休業中", "春季休業中", "通年"]
+
+    for s in remove_list:
+           module = module.replace(s, "")
+           module = module.replace("春学期","春ABC")
+           module = module.replace("秋学期","秋ABC")
+       
+    return module
+
+
+def remove_special_period(period):
+
+    remove_list = ["集中", "応談", "随時", ",集中", ",応談", ",随時"]
+
+    for s in remove_list:
+        period = period.replace(s, "")
+    return period
+
+
+def get_separate_index(period):
+    
+    separate_index_list = []
+
+    for i in range(len(period)):
+        if period[i] in WEEKDAY_LIST:
+            separate_index_list.append(i)
+
+    separate_index_list.pop(0)
+    return separate_index_list
+
+
+def get_num_index(period):
+
+    count = 0
+
+    for i in range(len(period)):
+        try:
+            tmp = int(period[i])
+            return count
+            
+        except ValueError:
+            count += 1
+        
+
+def apply_num_info(period, num_info):
+
+    period = list(period)
+
+    #Ex: 月・火1
+    if "・" in period:
+        period = [s.replace("・", num_info + ",") for s in period]
+
+    else:
+
+        for i in range(len(period)):
+            if period[i] in WEEKDAY_LIST:
+                period[i] += num_info
+
+    return "".join(period)
+
+
+
+def is_split_period(period):
+
+    splited_period = period.split(",")
+    for e in splited_period:
+        try:
+            tmp = int(e)
+            return False
+        except ValueError:
+            pass
+    return True
+
+
+def is_blank_list(l):
+
+    l = list(filter(lambda x: x != "", l))
+    return l == []
+
+
+def is_int(n):
+    try:
+        int(n)
+        return True
+    
+    except ValueError:
+        return False
+
+
+def count_num_info(period):
+    count = 0
+    for i in range (len(period) -1):
+        if period[i] == "," and period[i+1] in WEEKDAY_LIST:
+            count += 1
+    return count + 1
+            
+
+def split_period(period):
+    
+    period = list(period)
+
+    for i in range(len(period) - 1):
+        if period[i] == "," and period[i+1] in WEEKDAY_LIST:
+            period[i] = "."
+
+    return "".join(period).split(".")
+
+
+def merge_period(period):
+
+
+    tmp = []
+
+    for i in range(1, len(period)):
+        if is_int(period[i]):
+            tmp.append(period[i-1] + period[i])
+
+    return tmp
+
+
+def main():
+
+    module_list = []
+    period_list = []
+    course_list = []
+    course_tmp = []
+    filtered_course_list = []
+    class_list = create_class_list()
+    class_len = len(class_list)
+    # module_period_list: [module_list, period_list]
+
+    for c in class_list:
+        class_id = c.class_id
+        name = c.name
+        module = c.module
+        period = c.period
+        room = c.room
+        description = c.description
+
+        course_list.append([class_id, name, module, period, room, description])
+
+    for c in course_list:
+
+        class_id = c[0]
+        name = c[1]
+        module= c[2]
+        period = c[3]
+        room = c[4]
+        description = c[5]
+
+
+        module_tmp = module.split(" ")
+        period_tmp = period.split(" ")
+
+
+
+        module_len = len(module_tmp)
+        period_len = len(period_tmp)
+
+        if module_len == period_len:
+
+            for i in range(module_len):
+
+                if is_multiple_terms(module_tmp[i]):
+
+                    split_index = module.find("秋")
+                    module_list.append(module_tmp[i][0:split_index])
+                    module_list.append(module_tmp[i][split_index:])
+                    period_list.append(period_tmp[i])
+                    period_list.append(period_tmp[i])
+                module_list.append(module_tmp[i])
+                period_list.append(period_tmp[i])
+
+
+        
+        # In this case, period_len equals 1 e.g, 春C秋A:火2,3
+        elif module_len > period_len:
+
+            for i in range(module_len):
+
+                if is_multiple_terms(module_tmp[i]):
+                    split_index = module.find("秋")
+                    module_list.append(module_tmp[i][0:split_index])
+                    module_list.append(module_tmp[i][split_index:])
+                    period_list.append(period_tmp[0])
+                    period_list.append(period_tmp[0])
+
+                module_list.append(module_tmp[i])
+                period_list.append(period_tmp[0])
+
+
+        elif module_len == 1:
+            for i in range(period_len):
+                module_list.append(module_tmp[0])
+                period_list.append(period_tmp[i])
+
+            
+        else:
+            description = "error"
+
+        #The length of module_list and period_list should be the same
+        #assert len(module_list) == len(period_list)
+        print("{}:{}".format(module_list,period_list))
+
+        # Remove special modules
+        module_list = list(map(remove_special_module, module_list))
+        period_list = list(map(remove_special_period, period_list))
+
+    for i in range(class_len):
+        if  module_list[i] == "" or period_list[i] == "":
+            continue
+
+        else:
+            filtered_course_list.append([course_list[i][0],course_list[i][1],module_list[i], period_list[i],course_list[i][4], course_list[i][5]])
+
+    # Parse periods 
+    # Sunday classes are not available for now since there is no Sunday class in this year
+    # The lenths of module_list and period_list are the same
+
+
+    tmp2=[]
+
+    #Reset period_list
+    #period_list = []
+    #print(period_list)
+
+    for course in filtered_course_list :
+
+        class_id = course[0]
+        name = course[1]
+        module = course[2]
+        period = course[3]
+        room = course[4]
+        description = course[5]
+
+        if is_blank_list(period):
+            continue
+
+        #splited_period = period.split(",")
+        tmp1 = []
+
+        if days_count(period) == 1:
+            tmp1.append(period)
+
+        elif days_count(period) == count_num_info(period):
+            splited_period = split_period(period)
+            tmp1.append(splited_period)
+            
+        else:
+
+            splited_period = split_period(period) 
+            for element in splited_period:
+
+                if days_count(element) > 1:
+
+                    num_index = get_num_index(element)
+                    num_info = element[num_index:]
+                    new_element = apply_num_info(element, num_info).split(",")
+
+                    for p in new_element:
+                        tmp1.append(p)
+            
+                else:
+                    tmp1.append(element)
+            
+            #There exists a class period is ["1"], exclude this
+            if len(tmp1) == 1:
+                continue
+            #Check if the list contains number element
+            flag = False
+
+            for element in tmp1:
+                if is_int(element):
+                    flag = True
+           
+            if flag:
+               tmp1 = merge_period(tmp1)
+
+            if isinstance(tmp1[0], list):
+                tmp1 = tmp1[0]
+
+       # print("{}:{}:{}".format(name, module, tmp1))
+
+    sys.exit()
+
+
+if __name__ == "__main__":
+    main()
--- parse_twinc.py
+++ parse_twinc.py
@@ -1,21 +1,26 @@
-import sys
+import json
+import re
 
-#Convert CSV to JSON for twinc
+# Global consts
+global WEEKDAY_LIST, SEASONS
+WEEKDAY_LIST =  ["月","火","水","木","金","土","日"]
+SEASONS = ["春","秋"]
+
 
 class Class():
 
-    def __init__(self, class_id, name, module, period, room, description):
+    def __init__(self, class_id, name, module, period_tmp, room, description):
         self.class_id = class_id
         self.name = name
         self.module = module
-        self.period = period
+        self.period_tmp = period_tmp
         self.room = room
         self.description = description
+        self.terms = [] 
+        self.period = []
 
-    
-# Global consts
-global WEEKDAY_LIST 
-WEEKDAY_LIST =  ["月","火","水","木","金","土"]
+    def as_json(self):
+        return {"class_id": self.class_id, "name": self.name, "module": self.terms, "period": self.period, "room": self.room, "description": self.description}
 
 
 def create_class_list():
@@ -32,7 +37,7 @@ def create_class_list():
           class_id = tmp[1]
           name = tmp[3]
           module = tmp[11]
-          period = tmp[13]
+          period_tmp = tmp[13]
           room = tmp[15]
           description = tmp[21]
 
@@ -40,277 +45,124 @@ def create_class_list():
               room = " " 
         
           # Remove classes that are not opened in this year
-          if not "" in set([class_id, name, module, period, room, description]):
-              class_list.append(Class(class_id, name, module, period, room, description))
+          if not "" in set([class_id, name, module, period_tmp, room, description]):
+              class_list.append(Class(class_id, name, module, period_tmp, room, description))
 
         return class_list
 
 
-def is_multiple_terms(module):
-
-    if module.find("春") != -1 and module.find("秋") != -1:
-        return True
-
-    else:
-        return False
+def create_timetable():
+    timetable = []
+    for i in range(7):
+        timetable.append([False for i in range(8)])
+    return timetable
 
 
-def days_count(period):
+def parse_timetable(table):
 
-    count = 0
-
-    for s in period:
-        if s in WEEKDAY_LIST :
-            count += 1
-
-    return count
-    
-
-def remove_special_module(module):
-
-    remove_list = [" 夏季休業中", " 春季休業中", "夏季休業中", "春季休業中", "通年"]
-
-    for s in remove_list:
-           module = module.replace(s, "")
-           module = module.replace("春学期","春ABC")
-           module = module.replace("秋学期","秋ABC")
-       
-    return module
-
-
-def remove_special_period(period):
-
-    remove_list = ["集中", "応談", "随時", ",集中", ",応談", ",随時"]
-
-    for s in remove_list:
-        period = period.replace(s, "")
-    return period
-
-
-def get_separate_index(period):
-    
-    separate_index_list = []
-
-    for i in range(len(period)):
-        if period[i] in WEEKDAY_LIST:
-            separate_index_list.append(i)
-
-    separate_index_list.pop(0)
-    return separate_index_list
-
-
-def get_num_index(period):
-
-    count = 0
-
-    for i in range(len(period)):
-        flag = False
-        try:
-            tmp = int(period[i])
-            return count
-            
-        except ValueError:
-            count += 1
-        
-
-def apply_num_info(period, num_info):
-
-    period = list(period)
-
-    #Ex: 月・火1
-    if "・" in period:
-        period = [s.replace("・", num_info + ",") for s in period]
-
-    else:
-
-        for i in range(len(period)):
-            if period[i] in WEEKDAY_LIST:
-                period[i] += num_info
-
-    return "".join(period)
-
-
-
-def is_split_period(period):
-
-    splited_period = period.split(",")
-    for e in splited_period:
-        try:
-            tmp = int(e)
-            return False
-        except ValueError:
-            pass
-    return True
-
-
-def is_blank_list(l):
-
-    l = list(filter(lambda x: x != "", l))
-    return l == []
-
-
-def is_int(n):
-    try:
-        int(n)
-        return True
-    
-    except ValueError:
-        return False
-
-
-def count_num_info(period):
-    count = 0
-    for i in range (len(period) -1):
-        if period[i] == "," and period[i+1] in WEEKDAY_LIST:
-            count += 1
-    return count + 1
-            
-
-def split_period(period):
-    
-    period = list(period)
-
-    for i in range(len(period) - 1):
-        if period[i] == "," and period[i+1] in WEEKDAY_LIST:
-            period[i] = "."
-
-    return "".join(period).split(".")
-
-
-def main():
-
-    module_list = []
+    blank_table = create_timetable()
+    period = ""
     period_list = []
-    class_list = create_class_list()
-    class_len = len(class_list)
-    # module_period_list: [module_list, period_list]
-    module_period_list = []
 
-    for c in class_list:
-        class_id = c.class_id
-        name = c.name
-        module = c.module
-        period = c.period
-        room = c.room
-        description = c.description
-
-
-        module_tmp = module.split(" ")
-        period_tmp = period.split(" ")
-
-
-
-        module_len = len(module_tmp)
-        period_len = len(period_tmp)
-
-        if module_len == period_len:
-
-            for i in range(module_len):
-
-                if is_multiple_terms(module_tmp[i]):
-                    split_index = module.find("秋")
-                    module_list.append(module_tmp[i][0:split_index])
-                    module_list.append(module_tmp[i][split_index:])
-                    period_list.append(period_tmp[i])
-                    period_list.append(period_tmp[i])
-
-                else:
-                    module_list.append(module_tmp[i])
-                    period_list.append(period_tmp[i])
-
-        
-        # In this case, period_len equals 1 e.g, 春C秋A:火2,3
-        elif module_len > period_len:
-
-            for i in range(module_len):
-                
-                if is_multiple_terms(module_tmp[i]):
-                    split_index = module.find("秋")
-                    module_list.append(module_tmp[i][0:split_index])
-                    module_list.append(module_tmp[i][split_index:])
-                    period_list.append(period_tmp[0])
-                    period_list.append(period_tmp[0])
-
-                else:
-                    module_list.append(module_tmp[i])
-                    period_list.append(period_tmp[0])
-
-
-        elif module_len == 1:
-            for i in range(period_len):
-                module_list.append(module_tmp[0])
-                period_list.append(period_tmp[i])
-
-            
+    if table["period"] == blank_table:
+        if table["focus"]:
+            return ["集中"]
+        elif table["negotiable"]:
+            return ["応談"]
         else:
-            description = "error"
+            return ["随時"]
 
-        #The length of module_list and period_list should be the same
-        assert len(module_list) == len(period_list)
-
-        # Remove special modules
-        module_list = list(map(remove_special_module, module_list))
-        period_list = list(map(remove_special_period, period_list))
-
-    for i in range(class_len):
-        if  module_list[i] == "" or period_list[i] == "":
-            continue
-
-        else:
-            module_period_list.append([module_list[i], period_list[i]])
-
-    # Parse periods 
-    # Sunday classes are not available for now since there is no Sunday class in this year
-    # The lenths of module_list and period_list are the same
-
-
-    tmp2=[]
-
-    #Reset period_list
-    #period_list = []
-    #print(period_list)
-
-    for module_period in module_period_list :
-        period = module_period[1]
-
-        if is_blank_list(period):
-            continue
-
-        #splited_period = period.split(",")
-        tmp1 = []
-
-        if days_count(period) == 1:
-            tmp1.append(period)
-
-        elif days_count(period) == count_num_info(period):
-            splited_period = split_period(period)
-            tmp1.append(splited_period)
-            
-        else:
-
-            splited_period = split_period(period) 
-            for element in splited_period:
-
-                if days_count(element) > 1:
-
-                    num_index = get_num_index(element)
-                    num_info = element[num_index:]
-                    new_element = apply_num_info(element, num_info).split(",")
-
-                    for p in new_element:
-                        tmp1.append(p)
-            
-                else:
-                    tmp1.append(element)
-            
-            print(tmp1)
-
+    for i in range(7):
+        for j in range(8):
+            if table["period"][i][j]:
+                period = WEEKDAY_LIST[i] + str(j + 1)
+                period_list.append(period)
     
+    return period_list
 
 
+subject_map = {}
 
-    sys.exit()
+class_list = create_class_list()
 
+for course in class_list:
 
-if __name__ == "__main__":
-    main()
+    term_groups = course.module.split(" ")
+    season = ""
+
+    for group_str in term_groups:
+        group = []
+        char_array = list(group_str)
+
+        for char in char_array:
+
+            if char in SEASONS:
+                season = char
+
+            if season != "":
+                if char in ["A", "B", "C"]:
+                    if season == "春":
+                        no = 0
+                    else:
+                        no = 3
+                    
+                    if char == "A":
+                        no += 0
+                    elif char == "B":
+                        no += 1
+                    else:
+                        no += 2
+                    group.append(no)
+
+                if char == "休":
+                    group.append(SEASONS.index(season) + 6)
+
+        course.terms.append(group)
+    
+    term_str_array = course.period_tmp.split(" ")
+
+    for i in range(len(term_str_array)):
+        term = term_str_array[i]
+        period_str_array = term.split(",")
+        day_array = []
+        course.period.append({"focus": term.find("集中") > -1, "negotiable": term.find("応談") > -1, "asneeded": term.find("随時") > -1, "period": create_timetable()})
+
+        for p in period_str_array:
+            day_str = re.sub("[0-9\-]", "", p)
+            days = day_str.split("・")
+            days = list(filter(lambda x: x in WEEKDAY_LIST, days))
+            days = list(map(lambda x: WEEKDAY_LIST.index(x), days))
+
+            if len(days) > 0:
+                day_array = days
+
+            time_array = []
+            time_str = re.sub("[^0-9\-]", "", p)
+
+            if time_str.find("-") > -1:
+                time_str_array = time_str.split("-")
+                start_time = int(time_str_array[0])
+                end_time = int(time_str_array[1])
+                for j in range(start_time, end_time + 1, 1):
+                    time_array.append(j)
+            
+            else:
+                if time_str != "":
+                    time_array.append(int(time_str))
+                else:
+                    time_array.append("")
+
+            if len(time_str) > 0:
+                for day in day_array:
+                    for  time in time_array:
+                        course.period[i]["period"][day][time-1] = True
+
+    for i in range(len(course.period)):
+        course.period[i] = parse_timetable(course.period[i])
+
+    subject_map[course.class_id] = course.as_json()
+
+enc = json.dumps(subject_map,ensure_ascii=False)
+with open("kdb_twinc.json", "w") as f:
+    f.write(enc)
+print("complete")
