d83677bb1cf962175506c21b4deb8b207139c72d
Mimori256 <mimorimori256@gmail.com>
2022-03-23 13:25:14 +0900

    #2 を修正, kdb.jsonのフォーマットを変更

--- parse.py
+++ parse.py
@@ -12,8 +12,10 @@ with open("kdb.csv", "r", encoding="utf_8") as f:
         if tmp1[15] == "":
             tmp1[15] = " " 
 
-        if not "" in set([tmp1[1], tmp1[3], tmp1[11], tmp1[13], tmp1[15], tmp1[21]]):
-            l.append([tmp1[1], tmp1[3], tmp1[11], tmp1[13], tmp1[15], tmp1[21]])
+        unit = tmp1[7]
+        unit = unit.replace(" ", "")
+        if not "" in set([tmp1[1], tmp1[3], tmp1[11], tmp1[13], tmp1[15], tmp1[21], unit]):
+            l.append([tmp1[1], tmp1[3], tmp1[11], tmp1[13], tmp1[15], tmp1[21], unit])
 
 json_data = {}
 l.pop(0)
@@ -21,7 +23,8 @@ l.pop(0)
 for i in l:
     json_data[i[0]] = i[1:]
 
-enc = json.dumps(json_data,ensure_ascii=False)
+#{ "科目番号": ["科目名","モジュール","曜時限","教室","備考","単位数"], ・・・}
+enc = json.dumps(json_data, ensure_ascii=False, indent=2)
 
 with open("kdb.json", "w") as f:
     f.write(enc)
